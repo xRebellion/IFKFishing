9642c15c680268d62d9c454d73126a702c4c7cd2
Daniel- <danielyg2904@gmail.com>
2020-04-23 11:50:18 +0700

    update notification message

--- src/main/java/me/rebellion/ifkfishing/managers/NotificationManager.java
+++ src/main/java/me/rebellion/ifkfishing/managers/NotificationManager.java
@@ -5,13 +5,14 @@ import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 public class NotificationManager {
-    private static final String NOTIFICATION_MESSAGE = "&aAFK-Fishing detected for %PLAYER% on X: %X% Y: %Y% Z: %Z%. Putting them to sleep...";
+    private static final String NOTIFICATION_MESSAGE = "&aAFK-Fishing detected for %PLAYER% on X: %X% Y: %Y% Z: %Z%. Streaks: %STREAK%";
 
-    public static void notifyStaff(Player p) {
+    public static void notifyStaff(Player p, int streak) {
         String message = NOTIFICATION_MESSAGE.replace("%PLAYER%", p.getName())
                 .replace("%X%", Integer.toString(p.getLocation().getBlockX()))
                 .replace("%Y%", Integer.toString(p.getLocation().getBlockY()))
-                .replace("%Z%", Integer.toString(p.getLocation().getBlockZ()));
+                .replace("%Z%", Integer.toString(p.getLocation().getBlockZ()))
+                .replace("%STREAK%", Integer.toString(streak));
 
         message = ChatColor.translateAlternateColorCodes('&', message);
 
